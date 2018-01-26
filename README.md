762709ae28da9f965fdac5ac19a94681dcdb2d7c
jerry <gawryluklukasz@gmail.com>
2018-01-26 13:31:44 +0100

    insert project

---
+++ README.md
@@ -0,0 +1,19 @@
+Hot potato api
+===========================
+
+Installation 
+-------------------------
+
+1. Clone project 
+2. composer install
+
+Running application 
+-------------------------
+
+Tu run application we just `php -S 127.0.0.1:8000 -t public`
+
+Documentation 
+-------------------------
+
+After run application we can check documentation on link:
+http://127.0.0.1:8000/api/docs
---
+++ src/Entity/Question.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\QuestionRepository")
+ * @ApiResource(
+ *      attributes={"filters"={"question.search_filter"}},
+ *      collectionOperations={"get"={"method"="GET"}},
+ *      itemOperations={"get"={"method"="GET"}}
+ * )
+ * @author Łukasz Gawryluk <gawryluklukasz@gmail.com>
+ */
+class Question
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * Set base field
+     */
+    public function __construct()
+    {
+        $this->created = new DateTime();
+    }
+
+    /**
+     * @return int
+     */
+    function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @return DateTime
+     */
+    function getCreated(): DateTime
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param string $text
+     */
+    function setText($text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @param DateTime $created
+     */
+    function setCreated(DateTime $created)
+    {
+        $this->created = $created;
+    }
+}
